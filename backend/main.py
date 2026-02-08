08e7b09c04052f6737fb4addac1910c14cced25f
andlit0vv <andlit0vv@gmail.com>
2026-02-08 14:41:48 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -5,3 +5,9 @@ app = Flask(__name__)
 @app.route("/ping")
 def ping():
     return {"status": "ok"}
+
+@app.route("/auth/telegram", methods=["POST"])
+def auth_telegram():
+    data = request.json
+    print("RAW DATA FROM FRONTEND:", data)
+    return jsonify({"status": "ok"})
