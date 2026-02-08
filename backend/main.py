4fad11a1e0daada07cf8d1f0d13bb7894fc7f18a
andlit0vv <andlit0vv@gmail.com>
2026-02-09 01:28:11 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from urllib.parse import parse_qs
+import json
 
 app = Flask(__name__)
 CORS(app)
@@ -10,6 +12,13 @@ def ping():
 
 @app.route("/auth/telegram", methods=["POST"])
 def auth_telegram():
-    data = request.json
-    print("RAW DATA FROM FRONTEND:", data)
+    init_data = request.json.get("initData")
+
+    parsed = parse_qs(init_data)
+    user = json.loads(parsed["user"][0])
+
+    print("TELEGRAM USER ID:", user["id"])
+    print("USERNAME:", user.get("username"))
+    print("FIRST NAME:", user.get("first_name"))
+
     return jsonify({"status": "ok"})
